7e0ccaf6a69b52f630bd0419e3d2ee1753bd1bfe
tracyw904 <143651054+tracyw904@users.noreply.github.com>
2023-09-13 15:56:06 -0400

    vxv

---
+++ main.py
@@ -0,0 +1,13 @@
+fruit = "Apples, Oranges"
+berries = "Blueberries, Strawberries, Raspberries"
+stoneFruit = "Peaches, Plums"
+
+print(berries + ", " + fruit + ", " + stoneFruit)
+
+#if you dont list variable as a string then it will only print the assigned elements 2.9.2
+
+fruit = "Apples, Oranges"
+berries = "Blueberries, Strawberries, Raspberries"
+stoneFruit = "Peaches, Plums"
+
+print(fruit + ", " + berries + ", " + stoneFruit)
