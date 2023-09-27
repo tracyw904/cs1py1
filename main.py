ee15ba7707104e6a4e8c4bcdd88c60f08f0f4dcc
tracyw904 <143651054+tracyw904@users.noreply.github.com>
2023-09-27 15:53:08 -0400

    dsadczcz

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+''' 9/25/2023
 pi = 3.141
 r = float(input("Please enter value: ")) 
 print(r)
@@ -5,3 +6,9 @@ print(r)
 area = pi*r*r 
 
 print(area)
+'''
+#9/27/2023 LOOPS
+
+x = input()
+for r in range(len(x)):  
+    print(x[r])
