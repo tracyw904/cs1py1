3069997aeb93bde6b3b7b20acddb3aa6cd329213
tracyw904 <143651054+tracyw904@users.noreply.github.com>
2023-10-02 15:55:19 -0400

    cbvcbvcbn

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-''' 9/25/2023
-pi = 3.141
-r = float(input("Please enter value: ")) 
-print(r)
-
-area = pi*r*r 
-
-print(area)
 '''
-#9/27/2023 LOOPS
+10/2 booleans, comparison operators 
+x = 5
+print(x==5)
+print(x!=4)
+print(x==6)
+print(x!=6)
+'''
+#else and if statements
+'''
+x=5
+if x==100:
+    print("yes this is true")
+else:
+    print("slay")
+print("completed")
 
-x = input()
-for r in range(len(x)):  
-    print(x[r])
+#print values -- range for all numbers, elseif, conditions
+for x in range(10):
+    #print(x)    
+    if x%2== 1:
+        print(x, " is an odd number")
+    else:
+        print(x, " is an even number")
+        '''
+
+# NESTED condition!
+for x in range(10):
+    #print(x)    
+    if x%2== 1:
+            if x>5:
+                print("x, this is larger than 5")
+    print(x, " is an odd number")
+else:
+    print(x, " is an even number")
